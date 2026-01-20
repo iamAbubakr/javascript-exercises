948cfadaa9518a587e7e4858eca2983baec1fefb
shomanzar <sshoazimzoda@zinda.io>
2026-01-20 16:13:42 +0500

    factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,15 @@
-const factorial = function() {
-
+const factorial = function (n) {
+    if (n === 0) {
+        return 1;
+    }
+    else if (n < 0) {
+        return undefined;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
 };
+console.log(factorial(6));
 
 // Do not edit below this line
 module.exports = factorial;
